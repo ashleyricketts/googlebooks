daea56914ec4eaa4cd761985921b568949199e5a
Ashley Ricketts <aricketts12@gmail.com>
2019-06-17 17:43:08 -0500

    starting over

--- client/src/components/navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Navbar() {
-    return (
-        <nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand" href="#">Google Book Search</a>
-        </nav>
-    );
-}
--- client/src/components/searchbooks.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-<form className="form-inline">
-    <div className="form-group mb-2">
-        <label for="bookSearch">Book Title: </label>
-        <input type="text" className="form-control form-contol-lg"></input>
-        <button type="submit" className="btn btn primary mb-2">Submit</button>
-    </div>
-</form>
